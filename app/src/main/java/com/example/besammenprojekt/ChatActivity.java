8fafd887e657d6cd7befd0c8fc806f298c6b5581
Alisashala <alis0216@tud.kea.dk>
2023-05-22 08:34:49 +0200

    Design layout --> login, sign up, chat

--- app/src/main/java/com/example/besammenprojekt/ChatActivity.java
+++ app/src/main/java/com/example/besammenprojekt/ChatActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class FrontPageActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+
+public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_front_page);
+        setContentView(R.layout.activity_chat);
+
     }
 }
