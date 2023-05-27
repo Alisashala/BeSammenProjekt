0b5fe82ea526f4a3be930aeea045e8712313f794
Rxyxna <Rayanabaga@outlook.com>
2023-05-27 18:53:29 +0200

    #74 --> comments til at + chatactivity og loginactivitytest

--- app/src/main/java/com/example/besammenprojekt/Chat/ChatActivity.java
+++ app/src/main/java/com/example/besammenprojekt/Chat/ChatActivity.java
@@ -27,6 +27,7 @@ import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ktx.Firebase;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -45,88 +46,115 @@ import nl.dionsegijn.konfetti.xml.KonfettiView;
 
 public class ChatActivity extends AppCompatActivity {
 
+// Disse variabler repræsenterer forskellige elementer i brugergrænsefladen, autentifikation
+// og håndtering af animationer.
+
+    // User interface elementer / brugerfladeelementer
     private ListView lv;
     private Button send;
-    private Button backBtn;
-    private FirebaseAuth mAuth;
-    private EditText ed;
-    private KonfettiView confettiView = null;
 
-    private Shape.DrawableShape drawableShape = null;
-    private Party party = null;
+    private EditText ed;
+    private KonfettiView confettiView;
     private ProgressBar progressBar;
-    private Handler handler;
-    private Runnable runnable;
+
+
+    // Grafik og animation
+    private Party party;
+
+    // Handler og Runnable
+    private Handler handler; // Handler til udførelse af forsinkede handlinger
+    private Runnable runnable; // Runnable til definerende opgaver, der skal udføres
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        send = findViewById(R.id.sendButton);
-        FirebaseDatabase db = FirebaseDatabase.getInstance();
-        ArrayList al = new ArrayList();
-        lv = findViewById(R.id.lv);
-        ed = findViewById(R.id.inputMessage);
-        confettiView = findViewById(R.id.confettiView);
-        progressBar = findViewById(R.id.progressBar);
-        // Initialize the Handler
-        handler = new Handler();
 
-        // Create a Runnable to hide the progress bar
+// initialisering af elementer, og tilknytning til de tilsvarende variabler
+
+        // User interface elementer / brugerfladeelementer --> initialisering
+        send = findViewById(R.id.sendButton); // Knappen til at sende beskeder
+        lv = findViewById(R.id.lv); // ListView til visning af beskeder
+        ed = findViewById(R.id.inputMessage); // EditText til indtastning af beskeder
+        confettiView = findViewById(R.id.confettiView); // KonfettiView til visning af konfetti-animation
+        progressBar = findViewById(R.id.progressBar); // ProgressBar til angivelse af fremskridt
+
+        // Database-initialisering
+        FirebaseDatabase db = FirebaseDatabase.getInstance(); // Initialiserer FirebaseDatabase
+
+        // Dataopbevaring
+        ArrayList al = new ArrayList(); // ArrayListe til at gemme data
+
+        // Initialisering af handler
+        handler = new Handler(); // Handler til udførelse af forsinkede handlinger
+
+
+// Runnable-ínstans
+       //  En ny Runnable-instans oprettes med en implementering af run()-metoden. I dette tilfælde
+       // skjuler metoden progressbaren ved at ændre dens synlighed til View.GONE.
         runnable = new Runnable() {
             @Override
             public void run() {
                 progressBar.setVisibility(View.GONE);
             }
         };
-        // Show the progress bar
+        // Viser progressbaren
         progressBar.setVisibility(View.VISIBLE);
-        // Schedule the runnable to hide the progress bar after 7 seconds
+
+        // handler.postDelayed()-metoden bruges til at planlægge kørslen af runnable-objektet
+        // med en forsinkelse på 3000 millisekunder (3 sekunder).
         handler.postDelayed(runnable, 3000);
 
 
 
+        // Denne linje opretter en ChildEventListener, der lytter efter ændringer i child elememter
+        // under "Messages" i databasen.
         db.getReference("Messages").addChildEventListener(new ChildEventListener() {
-
 
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                // Viser en toast-besked når der tilføjes en besked
                 Toast.makeText(ChatActivity.this, "Message added"+snapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
+
+                // Tilføjer den modtagne besked til arraylisten
                 al.add(snapshot.getValue().toString());
+
+                // Opretter en ArrayAdapter med arraylisten og tilknytter den til ListView
                 ArrayAdapter<String> adapter = new ArrayAdapter(ChatActivity.this, android.R.layout.simple_list_item_1, al){
                     @NonNull
                     @Override
+                    // getView()-metoden opretter og tilpasser visningen for hvert element i ListView.
+                    // Den kalder først super.getView(position, convertView, parent) for at få standardvisningen.
+                    // Derefter tilpasses visningen baseret på elementets position.
                     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
                         View view = super.getView(position, convertView, parent);
 
+                        // Skifter baggrundsfarven for hver tredje og fjerde besked
                         if(position %3 == 1 || position %4 == 2) {
-                            // Inflate the XML layout file containing the desired TextView
+                            // Inflater XML-layoutfilen med den ønskede TextView
                             LayoutInflater inflater = LayoutInflater.from(getContext());
                             View layout = inflater.inflate(R.layout.activity_chat, parent, false);
 
-                            // Find the specific TextView in the inflated layout
+                            // Finder den specifikke TextView i den indflatede layout
                             TextView textView = layout.findViewById(R.id.editListView);
 
-                            // Set the desired background for the TextView
+                            // Sætter den ønskede baggrund for TextView'en
                             textView.setBackgroundResource(R.drawable.background_sent_message);
 
-                            // Replace the original view's background with the inflated layout
+                            // Erstatter den oprindelige views baggrund med den indflatede layout
                             view.setBackground(textView.getBackground());
                         } else {
+                            // Sætter baggrundsfarven for de øvrige beskeder
                             view.setBackgroundResource(R.drawable.background_recieved_message);
                         }
                         return view;
                         }
                 };
-
+                // Tilføjer adapteren til ListView
                 lv.setAdapter(adapter);
             }
-
-
-
-
-
 
 
             @Override
@@ -149,34 +177,59 @@ public class ChatActivity extends AppCompatActivity {
 
             }
         });
+
+// onClick til sendknappen
+        //Sætter en OnClickListener til knappen send og definerer dens onClick-metode
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Opretter en instans af FirebaseAuth ved at kalde FirebaseAuth.getInstance().
+                // Dette giver adgang til Firebase Authentication-funktionaliteterne.
                 FirebaseAuth a = FirebaseAuth.getInstance();
+
+                // Her oprettes en instans af Date ved at kalde Calendar.getInstance().getTime().
+                // Dette bruges til at få den aktuelle dato og tidspunkt.
                 Date getdate = Calendar.getInstance().getTime();
+
+                // Gemmer indtastet tekst i Firebase Realtime Database under "Messages" med en unik
+                // nøgle bestående af brugerens UID og det aktuelle dato og tidspunkt.
                 db.getReference("Messages").child(a.getUid()+getdate).setValue(ed.getText().toString());
             }
         });
 
-
-
     }
+
+
+// Konfettiknap
+
 
     public void showConfetti(View view) {
 
+            // Konfigurerer en emitter, der udsender partikler med en hastighed på 1000 partikler
+            // pr. sekund i en periode på 350 millisekunder.
             EmitterConfig emitterConfig = new Emitter(350, TimeUnit.MILLISECONDS).perSecond(1000);
+
+            //Opretter en konfetti-parti ved hjælp af en PartyFactory med den konfigurerede emitter.
+            // Der er yderligere indstillinger som spredning, farver og hastighedsinterval.
             party = new PartyFactory(emitterConfig)
+                    // konfetti farver, spread, position osv.
                     .spread(700)
                     .colors(Arrays.asList(0xff3660, 0x4D7429, 0xB18F6A, 0x1790d0, 0xffffba, 0xbaffc9))
                     .setSpeedBetween(0f, 30f)
                     .position(new Position.Relative(0.5, 0.3))
+                    // Bygger og returnerer et færdigt party-objekt baseret på de tidligere indstillinger.
                     .build();
 
+            // Starter konfettianimationen ved at give party-objektet til confettiView til visning
+        // af konfetti.
             confettiView.start(party);
         }
 
-    //backbutton from chatActivity til AgeActivity, tjek onclick i xml filen.
+// Backbutton from chatActivity til AgeActivity
+
     public void backButton(View view) {
+
+        // intent --> oprettelse af intent for at navigere tilbage til AgeActivity
         Intent intentToAgeActivity = new Intent(getApplicationContext(), AgeActivity.class);
         startActivity(intentToAgeActivity);
         finish();
--- app/src/main/java/com/example/besammenprojekt/Onboarding/SignUpActivity.java
+++ app/src/main/java/com/example/besammenprojekt/Onboarding/SignUpActivity.java
@@ -23,6 +23,8 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    // Definerer private variabler af forskellige typer, der bruges til at håndtere
+    // brugerens loginfunktion.
     private EditText editEmail;
     private EditText editPassword;
     private Button signUpBtn;
@@ -34,70 +36,119 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
+
+        // Tjekker om brugeren er logget ind (ikke-null) og opdaterer brugergrænsefladen (UI)
+        // Først hentes den aktuelle bruger fra Firebase Authentication ved hjælp af
+        // mAuth.getCurrentUser() metoden.
         FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        // Hvis brugeren er logget ind, startes MainActivity og loginskærmen afsluttes.
         if(currentUser != null){
+
             Intent intentToMain = new Intent(getApplicationContext(), MainActivity.class);
+
             startActivity(intentToMain);
+
             finish();
         }
     }
 
+    // Metode kaldt når aktiviteten oprettes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        // Initialiserer Firebase Authentication
         mAuth = FirebaseAuth.getInstance();
+
+        // Identificerer 'view'-komponenterne i layoutfilen ved hjælp af deres unikke id'er
+        // og tilordner dem til de tilsvarende variabler.
         editEmail = findViewById(R.id.et_email);
         editPassword = findViewById(R.id.et_password);
         signUpBtn = findViewById(R.id.btn_signup);
         progressBar = findViewById(R.id.progressBar);
         textView = findViewById(R.id.loginNow);
 
+
+        // Sætter en OnClickListener på textView-objektet.
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
+            // Når brugeren klikker på dette View, oprettes intent  for at navigere fra SignUpActivity
+            // til LoginActivity
             public void onClick(View v) {
                 Intent intentToLogin = new Intent(getApplicationContext(), LoginActivity.class);
+
+                // Starter LoginActivity og afslutter den nuværende aktivitet.
                 startActivity(intentToLogin);
                 finish();
             }
         });
 
+        // Angiver en onClick-listener til signUpBtn.
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Når signUpBtn bliver klikket, gøres progressBar-komponenten synlig.
                 progressBar.setVisibility(View.VISIBLE);
+
+                // - Email og password hentes fra editEmail- og editPassword-felterne
+                // og konverteres til strenge.
                 String email;
                 String password;
                 email = editEmail.getText().toString();
                 password = editPassword.getText().toString();
 
+                // - Hvis email-feltet er tomt, vises en kort besked om at indtaste en email
+                // ved hjælp af Toast.
                 if (TextUtils.isEmpty(email)){
                     Toast.makeText(SignUpActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                // - Hvis password-feltet er tomt, vises en kort besked om at indtaste et password
+                // ved hjælp af Toast.
                 if (TextUtils.isEmpty(password)){
                     Toast.makeText(SignUpActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
+                // Opretter en ny brugerkonto i Firebase Authentication ved hjælp af den angivne
+                // email og adgangskode.
                 mAuth.createUserWithEmailAndPassword(email, password)
+
+                        // Tilføjer en onCompleteListener til at lytte efter afslutningen af
+                        // oprettelsesprocessen.
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
+                                // Når oprettelsen er fuldført, skjules progressBar-komponenten.
                                 progressBar.setVisibility(View.GONE);
+
+                                // Hvis oprettelsen er succesfuld, vises en kort besked om,
+                                // at kontoen er oprettet.
                                 if (task.isSuccessful()) {
                                     Toast.makeText(SignUpActivity.this, "Account created",
                                             Toast.LENGTH_SHORT).show();
+
+                                    // Indsamler brugernavn fra det tilhørende tekstfelt.
                                     userName = findViewById(R.id.et_userName);
-                                    String message = userName.getText().toString(); // Get the string value entered by the user
+                                    String message = userName.getText().toString();
+
+                                    // Opretter en Intent til at navigere fra SignupActivity
+                                    // til MainActivity.
                                     Intent intentToMain = new Intent(SignUpActivity.this, MainActivity.class);
+
+                                    // Sender brugernavnet som en ekstra information med Intent.
+                                    //key = nøgle
+                                    //message = værdien
                                     intentToMain.putExtra("key", message);
+
+                                    // Starter MainActivity med Intenten.
                                     startActivity(intentToMain);
 
                                 } else {
-                                    // If sign in fails, display a message to the user.
+                                    // Hvis oprettelsen fejler, vises en kort besked om, at
+                                    // godkendelsen mislykkedes.
                                     Toast.makeText(SignUpActivity.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
                                 }
