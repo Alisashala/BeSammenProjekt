14a3d03f4508f2e71c22085c59830c4cd9111033
Rxyxna <Rayanabaga@outlook.com>
2023-05-27 19:20:55 +0200

    solving merging conflict

--- app/src/main/java/com/example/besammenprojekt/Chat/ChatActivity.java
+++ app/src/main/java/com/example/besammenprojekt/Chat/ChatActivity.java
@@ -1,17 +1,11 @@
 package com.example.besammenprojekt.Chat;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,26 +14,22 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.besammenprojekt.Details.RulesAndInformation;
-import com.example.besammenprojekt.MainActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.besammenprojekt.R;
 import com.example.besammenprojekt.User.AgeActivity;
-import com.example.besammenprojekt.User.UserActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.ktx.Firebase;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import nl.dionsegijn.konfetti.core.Party;
@@ -47,7 +37,6 @@ import nl.dionsegijn.konfetti.core.PartyFactory;
 import nl.dionsegijn.konfetti.core.Position;
 import nl.dionsegijn.konfetti.core.emitter.Emitter;
 import nl.dionsegijn.konfetti.core.emitter.EmitterConfig;
-import nl.dionsegijn.konfetti.core.models.Shape;
 import nl.dionsegijn.konfetti.xml.KonfettiView;
 
 public class ChatActivity extends AppCompatActivity {
@@ -58,6 +47,9 @@ public class ChatActivity extends AppCompatActivity {
     // User interface elementer / brugerfladeelementer
     private ListView lv;
     private Button send;
+
+    private EditText ed;
+    private KonfettiView confettiView;
     private ProgressBar progressBar;
 
 
@@ -74,9 +66,58 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+// initialisering af elementer, og tilknytning til de tilsvarende variabler
+
+        // User interface elementer / brugerfladeelementer --> initialisering
+        send = findViewById(R.id.sendButton); // Knappen til at sende beskeder
+        lv = findViewById(R.id.lv); // ListView til visning af beskeder
+        ed = findViewById(R.id.inputMessage); // EditText til indtastning af beskeder
+        confettiView = findViewById(R.id.confettiView); // KonfettiView til visning af konfetti-animation
+        progressBar = findViewById(R.id.progressBar); // ProgressBar til angivelse af fremskridt
+
+        // Database-initialisering
+        FirebaseDatabase db = FirebaseDatabase.getInstance(); // Initialiserer FirebaseDatabase
+
+        // Dataopbevaring
+        ArrayList al = new ArrayList(); // ArrayListe til at gemme data
+
+        // Initialisering af handler
+        handler = new Handler(); // Handler til udførelse af forsinkede handlinger
+
+
+// Runnable-ínstans
+        //  En ny Runnable-instans oprettes med en implementering af run()-metoden. I dette tilfælde
+        // skjuler metoden progressbaren ved at ændre dens synlighed til View.GONE.
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                progressBar.setVisibility(View.GONE);
+            }
+        };
+        // Viser progressbaren
+        progressBar.setVisibility(View.VISIBLE);
+
+        // handler.postDelayed()-metoden bruges til at planlægge kørslen af runnable-objektet
+        // med en forsinkelse på 3000 millisekunder (3 sekunder).
+        handler.postDelayed(runnable, 3000);
 
 
 
+        // Denne linje opretter en ChildEventListener, der lytter efter ændringer i child elememter
+        // under "Messages" i databasen.
+        db.getReference("Messages").addChildEventListener(new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                // Viser en toast-besked når der tilføjes en besked
+                Toast.makeText(ChatActivity.this, "Message added"+snapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
+
+                // Tilføjer den modtagne besked til arraylisten
+                al.add(snapshot.getValue().toString());
+
+                // Opretter en ArrayAdapter med arraylisten og tilknytter den til ListView
+                ArrayAdapter<String> adapter = new ArrayAdapter(ChatActivity.this, android.R.layout.simple_list_item_1, al){
                     @NonNull
                     @Override
                     // getView()-metoden opretter og tilpasser visningen for hvert element i ListView.
@@ -85,10 +126,24 @@ public class ChatActivity extends AppCompatActivity {
                     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
                         View view = super.getView(position, convertView, parent);
 
+                        // Skifter baggrundsfarven for hver tredje og fjerde besked
+                        if(position %3 == 1 || position %4 == 2) {
+                            // Inflater XML-layoutfilen med den ønskede TextView
+                            LayoutInflater inflater = LayoutInflater.from(getContext());
+                            View layout = inflater.inflate(R.layout.activity_chat, parent, false);
+
+                            // Finder den specifikke TextView i den indflatede layout
+                            TextView textView = layout.findViewById(R.id.editListView);
+
+                            // Sætter den ønskede baggrund for TextView'en
+                            textView.setBackgroundResource(R.drawable.background_sent_message);
+
+                            // Erstatter den oprindelige views baggrund med den indflatede layout
+                            view.setBackground(textView.getBackground());
+                        } else {
+                            // Sætter baggrundsfarven for de øvrige beskeder
                             view.setBackgroundResource(R.drawable.background_recieved_message);
                         }
-
-                        textView.setText((String) al.get(position));
                         return view;
                     }
                 };
@@ -137,36 +192,41 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
 
-            EmitterConfig emitterConfig = new Emitter(350, TimeUnit.MILLISECONDS).perSecond(1000);
+    }
 
-            //Opretter en konfetti-parti ved hjælp af en PartyFactory med den konfigurerede emitter.
-            // Der er yderligere indstillinger som spredning, farver og hastighedsinterval.
-            party = new PartyFactory(emitterConfig)
-                    // konfetti farver, spread, position osv.
-                    .spread(700)
-                    .colors(Arrays.asList(0xff3660, 0x4D7429, 0xB18F6A, 0x1790d0, 0xffffba, 0xbaffc9))
-                    .setSpeedBetween(0f, 30f)
-                    .position(new Position.Relative(0.5, 0.3))
-                    // Bygger og returnerer et færdigt party-objekt baseret på de tidligere indstillinger.
-                    .build();
 
-            // Starter konfettianimationen ved at give party-objektet til confettiView til visning
+// Konfettiknap
+
+
+    public void showConfetti(View view) {
+
+        // Konfigurerer en emitter, der udsender partikler med en hastighed på 1000 partikler
+        // pr. sekund i en periode på 350 millisekunder.
+        EmitterConfig emitterConfig = new Emitter(350, TimeUnit.MILLISECONDS).perSecond(1000);
+
+        //Opretter en konfetti-parti ved hjælp af en PartyFactory med den konfigurerede emitter.
+        // Der er yderligere indstillinger som spredning, farver og hastighedsinterval.
+        party = new PartyFactory(emitterConfig)
+                // konfetti farver, spread, position osv.
+                .spread(700)
+                .colors(Arrays.asList(0xff3660, 0x4D7429, 0xB18F6A, 0x1790d0, 0xffffba, 0xbaffc9))
+                .setSpeedBetween(0f, 30f)
+                .position(new Position.Relative(0.5, 0.3))
+                // Bygger og returnerer et færdigt party-objekt baseret på de tidligere indstillinger.
+                .build();
+
+        // Starter konfettianimationen ved at give party-objektet til confettiView til visning
         // af konfetti.
-            confettiView.start(party);
-        }
+        confettiView.start(party);
+    }
 
 // Backbutton from chatActivity til AgeActivity
 
     public void backButton(View view) {
 
+        // intent --> oprettelse af intent for at navigere tilbage til AgeActivity
+        Intent intentToAgeActivity = new Intent(getApplicationContext(), AgeActivity.class);
         startActivity(intentToAgeActivity);
         finish();
     }
-
-    public void infoChatButton (View view) {
-        Intent intentToRulesAndIndformation = new Intent(ChatActivity.this, RulesAndInformation.class);
-        startActivity(intentToRulesAndIndformation);
-        finish();
-    }
-
 }
